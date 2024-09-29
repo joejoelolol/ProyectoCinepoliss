a955b08e312db1eb0f0a74c95ff7d5b9bd19957c
Alberto Palma <jesus.palma247910@potros.itson.edu.mx>
2024-09-28 21:09:05 -0700

    FrmReporte se ocupa testear

---
+++ src/main/java/persistencia/PersistenciaException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistencia;
+
+/**
+ *
+ * @author Alberto Palma
+ */
+public class PersistenciaException extends Exception{
+    /**
+     * Construye la excepción de persistencia
+     * @param msj el mensaje
+     */
+    public PersistenciaException(String msj){
+        super(msj);
+    }
+}
