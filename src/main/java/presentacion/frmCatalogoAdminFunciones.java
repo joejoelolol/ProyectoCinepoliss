9766dcac93b9a014443a03fd802bf01bf0c1c45e
Alberto Palma <jesus.palma247910@potros.itson.edu.mx>
2024-09-28 23:37:29 -0700

    admin hecho, pero no testeado

--- src/main/java/presentacion/frmCatalogoAdminFunciones.java
+++ src/main/java/presentacion/frmCatalogoAdminFunciones.java
@@ -249,14 +249,20 @@ public class frmCatalogoAdminFunciones extends javax.swing.JFrame {
             this.pagina = 1;
             return;
         }
-        this.establecerTituloPaginacion();
-        this.cargarTablaClientes();
+//        this.establecerTituloPaginacion();
+//        this.cargarTablaClientes();
     }//GEN-LAST:event_btnPaginaAnteriorActionPerformed
 
     private void btnPaginaSiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPaginaSiguienteActionPerformed
-        this.pagina++;
-        this.establecerTituloPaginacion();
-        this.cargarTablaClientes();
+        if (this.pagina == LIMITE) {
+            this.pagina = LIMITE;
+            return;
+        }else{
+            this.pagina++;
+        }
+        
+//        this.establecerTituloPaginacion();
+//        this.cargarTablaClientes();
     }//GEN-LAST:event_btnPaginaSiguienteActionPerformed
 
 
